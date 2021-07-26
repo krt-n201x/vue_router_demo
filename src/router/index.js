2b5e7eec40371ca7f661392ddb7e16e3158fb9b7
krt-n201x <kitsada_t@cmu.ac.th>
2021-07-26 18:43:26 +0700

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+    props: (route) => ({
+      page: parseInt(route.query.page) || 0,
+      perPage: parseInt(route.query.perPage) || 10
+    })
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+   component: About,
   },
 ];
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+ <div class="events">
+    <EventCard v-for="event in events" :key="event._id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+import { watchEffect } from '@vue/runtime-core'
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  //receive page variable props in the component
+  name: 'EventList',
+  props: {
+    page: {
+      type: Number,
+      required: true
+    },
+    perPage: {
+      type: Number,
+      required: true
+    }
   },
-};
+  components: {
+    EventCard
+    // register it as a child component
+  },
+  data() {
+    return {
+      events: null,
+      totalEvents: 0 // <-- Added this to store totalEvents
+    }
+  },
+  created() {
+    watchEffect(() => {
+      EventService.getEvents(this.perPage, this.page)
+        .then((response) => {
+          this.events = response.data.data
+          this.totalEvents = response.headers['x-total-count'] // <-- Store it
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    })
+  }
+}
 </script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
+}
+</style>
