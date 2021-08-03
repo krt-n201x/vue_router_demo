934dda6dd751a0e3162e61c731aaa0da084b85af
krt-n201x <kitsada_t@cmu.ac.th>
2021-08-03 18:17:55 +0700

    add flash msg

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,9 +1,25 @@
 <template>
   <p>Edit the event here</p>
-  <button>EDIT ME!</button>
+  <button @click="editer">EDIT ME!</button>
 </template>
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore'],
+  methods: {
+    editer() {
+      this.GStore.flashMessage =
+        'You are seccessfully edited for ' + this.event.title
+      setTimeout(() => {
+        //After 3 seconds remove it
+        this.GStore.flashMessage = ''
+      }, 5000)
+      //Set a flash message to appear on the next page loaded which says
+      this.$router.push({
+        name: 'Home',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
